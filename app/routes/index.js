398557fe1bc0dde7fca13d1fd3a50887a5ed7b00
schuylermk <schuyler.m.k@gmail.com>
2016-04-03 17:43:28 -0700

    Adds dummy object for data in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+  var questions = [{
+    author: "Jim",
+    content: "How much is it?",
+    notes: "per month"
+  }, {
+    author: "Liina",
+    content: "Where is it?",
+    notes: "What area"
+  }, {
+    author: "Jordan",
+    content: "What is in it for me?",
+    notes: "What do I get?"
+  }];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
